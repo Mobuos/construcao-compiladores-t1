4b179105b6875075ede6da66253cc2e85302386f
Vitor Recoaro <vitordearec@gmail.com>
2023-05-23 10:20:09 -0300

    add: T1Lexer

--- t1-lexico/src/main/antlr4/br/ufscar/dc/compiladores/t1/lexico/.antlr/T1Lexer.java
+++ t1-lexico/src/main/antlr4/br/ufscar/dc/compiladores/t1/lexico/.antlr/T1Lexer.java
@@ -16,7 +16,7 @@ public class T1Lexer extends Lexer {
 	protected static final PredictionContextCache _sharedContextCache =
 		new PredictionContextCache();
 	public static final int
-		COMENTARIO=1;
+		COMENTARIO=1, ALGORITMO=2, DECLARE=3, LITERAL=4, INTEIRO=5;
 	public static String[] channelNames = {
 		"DEFAULT_TOKEN_CHANNEL", "HIDDEN"
 	};
@@ -27,19 +27,20 @@ public class T1Lexer extends Lexer {
 
 	private static String[] makeRuleNames() {
 		return new String[] {
-			"COMENTARIO"
+			"COMENTARIO", "ALGORITMO", "DECLARE", "LITERAL", "INTEIRO"
 		};
 	}
 	public static final String[] ruleNames = makeRuleNames();
 
 	private static String[] makeLiteralNames() {
 		return new String[] {
+			null, null, "'algoritmo'", "'declare'", "'literal'", "'inteiro'"
 		};
 	}
 	private static final String[] _LITERAL_NAMES = makeLiteralNames();
 	private static String[] makeSymbolicNames() {
 		return new String[] {
-			null, "COMENTARIO"
+			null, "COMENTARIO", "ALGORITMO", "DECLARE", "LITERAL", "INTEIRO"
 		};
 	}
 	private static final String[] _SYMBOLIC_NAMES = makeSymbolicNames();
@@ -117,11 +118,21 @@ public class T1Lexer extends Lexer {
 	}
 
 	public static final String _serializedATN =
-		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\3\17\b\1\4\2\t\2"+
-		"\3\2\3\2\7\2\b\n\2\f\2\16\2\13\13\2\3\2\3\2\3\2\3\t\2\3\3\3\3\2\2\2\17"+
-		"\2\3\3\2\2\2\3\5\3\2\2\2\5\t\7}\2\2\6\b\13\2\2\2\7\6\3\2\2\2\b\13\3\2"+
-		"\2\2\t\n\3\2\2\2\t\7\3\2\2\2\n\f\3\2\2\2\13\t\3\2\2\2\f\r\7\177\2\2\r"+
-		"\16\b\2\2\2\16\4\3\2\2\2\4\2\t\3\3\2\2";
+		"\3\u608b\ua72a\u8133\ub9ed\u417c\u3be7\u7786\u5964\2\79\b\1\4\2\t\2\4"+
+		"\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\3\2\3\2\7\2\20\n\2\f\2\16\2\23\13\2\3\2"+
+		"\3\2\3\2\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\3\4\3\4\3\4\3\4\3\4\3"+
+		"\4\3\4\3\4\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\5\3\6\3\6\3\6\3\6\3\6\3\6\3\6"+
+		"\3\6\3\21\2\7\3\3\5\4\7\5\t\6\13\7\3\2\2\29\2\3\3\2\2\2\2\5\3\2\2\2\2"+
+		"\7\3\2\2\2\2\t\3\2\2\2\2\13\3\2\2\2\3\r\3\2\2\2\5\27\3\2\2\2\7!\3\2\2"+
+		"\2\t)\3\2\2\2\13\61\3\2\2\2\r\21\7}\2\2\16\20\13\2\2\2\17\16\3\2\2\2\20"+
+		"\23\3\2\2\2\21\22\3\2\2\2\21\17\3\2\2\2\22\24\3\2\2\2\23\21\3\2\2\2\24"+
+		"\25\7\177\2\2\25\26\b\2\2\2\26\4\3\2\2\2\27\30\7c\2\2\30\31\7n\2\2\31"+
+		"\32\7i\2\2\32\33\7q\2\2\33\34\7t\2\2\34\35\7k\2\2\35\36\7v\2\2\36\37\7"+
+		"o\2\2\37 \7q\2\2 \6\3\2\2\2!\"\7f\2\2\"#\7g\2\2#$\7e\2\2$%\7n\2\2%&\7"+
+		"c\2\2&\'\7t\2\2\'(\7g\2\2(\b\3\2\2\2)*\7n\2\2*+\7k\2\2+,\7v\2\2,-\7g\2"+
+		"\2-.\7t\2\2./\7c\2\2/\60\7n\2\2\60\n\3\2\2\2\61\62\7k\2\2\62\63\7p\2\2"+
+		"\63\64\7v\2\2\64\65\7g\2\2\65\66\7k\2\2\66\67\7t\2\2\678\7q\2\28\f\3\2"+
+		"\2\2\4\2\21\3\3\2\2";
 	public static final ATN _ATN =
 		new ATNDeserializer().deserialize(_serializedATN.toCharArray());
 	static {
--- t1-lexico/src/main/antlr4/br/ufscar/dc/compiladores/t1/lexico/T1Lexer.g4
+++ t1-lexico/src/main/antlr4/br/ufscar/dc/compiladores/t1/lexico/T1Lexer.g4
@@ -1,3 +1,9 @@
 lexer grammar T1Lexer;
 
 COMENTARIO: '{' .*?  '}' {skip()};
+ALGORITMO: 'algoritmo';
+DECLARE: 'declare';
+LITERAL: 'literal';
+INTEIRO: 'inteiro';
+
+
