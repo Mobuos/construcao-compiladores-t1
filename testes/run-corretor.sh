ea63314defc1300f8bd0b7c5f797261b2de63ecc
Vitor Recoaro <vitordearec@gmail.com>
2023-05-24 20:57:13 -0300

    fix: Corretor automático

--- testes/run-corretor.sh
+++ testes/run-corretor.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
-corretor=$PWD/corretor-automatico.jar
-# corretor=/home/recoaro/Codes/UFSCAR/2023/Compiladores/compiladores-corretor-automatico/target/compiladores-corretor-automatico-1.0-SNAPSHOT-jar-with-dependencies.jar
-executavel=$PWD/../t1-lexico/target/t1-lexico-1.0-SNAPSHOT-jar-with-dependencies.jar
+corretor=$PWD/testes/corretor-automatico.jar
+executavel=$PWD/t1-lexico/target/t1-lexico-1.0-SNAPSHOT-jar-with-dependencies.jar
 pastaTemp=$PWD/temp
-casosTeste=$PWD/casos-de-teste
+casosTeste=$PWD/testes/casos-de-teste
 ras="790716, 791964, 790035"
 opcao="gabarito-t1"
 
